b62fe3a39a4da8395082d53f4b41bd109a31070c
Diwen.Liu <demon90s@163.com>
2019-06-13 14:37:00 +0800

    Organize files

--- cpplabs/c11/constexpr.cpp
+++ cpplabs/c11/constexpr.cpp
@@ -4,6 +4,8 @@
     自C++11起， constexpr 可用来让表达式核定于编译期。
 */
 
+#include <iostream>
+
 constexpr int square(int x)
 {
     return x * x;
@@ -12,7 +14,7 @@ constexpr int square(int x)
 int main()
 {
     float a[square(9)];     // OK, 编译期就可以计算得到a有81个元素
-    (void)a;
+    std::cout << sizeof(a) / sizeof(a[0]) << std::endl; // 81
 
     return 0;
 }

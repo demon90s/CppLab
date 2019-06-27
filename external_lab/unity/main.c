dbce26b0c2fa0028c3909d630e973a99f0e9d1c9
Diwen.Liu <demon90s@163.com>
2019-06-27 16:08:35 +0800

    commit

--- external_lab/unity/main.c
+++ external_lab/unity/main.c
@@ -4,6 +4,23 @@
 void ohfuck(void)
 {
     TEST_FAIL();
+
+    printf("never!!\n");
+}
+
+void ohignore(void)
+{
+    TEST_IGNORE();
+
+    printf("never!!\n");
+}
+
+void ohassert(void)
+{
+    int a = 42;
+
+    TEST_ASSERT(a == 42);
+    TEST_ASSERT_EQUAL(42, a);
 }
 
 int main(void)
@@ -11,6 +28,8 @@ int main(void)
     UNITY_BEGIN();
 
     RUN_TEST(ohfuck);
+    RUN_TEST(ohignore);
+    RUN_TEST(ohassert);
 
     return UNITY_END();
 }

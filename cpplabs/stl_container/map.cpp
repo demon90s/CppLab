22e0d2e8b69b41d95cd3cd06911fbac992dc7655
Diwen.Liu <demon90s@163.com>
2019-11-11 01:38:47 +0800

    commit

--- cpplabs/stl_container/map.cpp
+++ cpplabs/stl_container/map.cpp
@@ -19,9 +19,61 @@ void test_custom_sort()
     }
 }
 
+// lower_bound(key)
+// 返回指向首个不小于(>=) key 的元素的迭代器。若找不到这种元素，则返回尾后迭代器
+void test_lower_bound()
+{
+    std::map<int, std::string> is_map = {
+        {1, "one"},
+        {10, "ten"},
+        {5, "five"}
+    };
+
+    int i;
+    while (std::cin >> i) {
+        auto it = is_map.lower_bound(i);
+        
+        if (it == is_map.end())
+        {
+            std::cout << "not match\n";
+        }
+        else
+        {
+            std::cout << "match: {" << it->first << " " << it->second << "}\n";
+        }
+    }
+}
+
+// lower_bound(key)
+// 返回指向首个大于 key 的元素的迭代器。若找不到这种元素，则返回尾后迭代器
+void test_upper_bound()
+{
+    std::map<int, std::string> is_map = {
+        {1, "one"},
+        {10, "ten"},
+        {5, "five"}
+    };
+
+    int i;
+    while (std::cin >> i) {
+        auto it = is_map.upper_bound(i);
+        
+        if (it == is_map.end())
+        {
+            std::cout << "not match\n";
+        }
+        else
+        {
+            std::cout << "match: {" << it->first << " " << it->second << "}\n";
+        }
+    }
+}
+
 int main()
 {
-    test_custom_sort();
+    //test_custom_sort();
+    //test_lower_bound();
+    test_upper_bound();
 
     return 0;
 }

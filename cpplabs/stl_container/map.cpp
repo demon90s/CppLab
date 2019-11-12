738af3c5013abb80d7525d2eab7762777f22f87f
Diwen.Liu <demon90s@163.com>
2019-11-12 13:23:45 +0800

    commit

--- cpplabs/stl_container/map.cpp
+++ cpplabs/stl_container/map.cpp
@@ -23,11 +23,15 @@ void test_custom_sort()
 // 返回指向首个不小于(>=) key 的元素的迭代器。若找不到这种元素，则返回尾后迭代器
 void test_lower_bound()
 {
-    std::map<int, std::string> is_map = {
-        {1, "one"},
-        {10, "ten"},
-        {5, "five"}
-    };
+    std::map<int, std::string, bool(*)(int, int)> is_map([](int lhs, int rhs) { return lhs > rhs; });   // 降序，逻辑反过来了，找首个 <= key 的元素
+
+    is_map.insert( {1, "one"});
+    is_map.insert( {10, "ten"});
+    is_map.insert( {5, "five"});
+
+    // 10 ten
+    // 5 five
+    // 1 one
 
     int i;
     while (std::cin >> i) {
@@ -72,8 +76,8 @@ void test_upper_bound()
 int main()
 {
     //test_custom_sort();
-    //test_lower_bound();
-    test_upper_bound();
+    test_lower_bound();
+    //test_upper_bound();
 
     return 0;
 }

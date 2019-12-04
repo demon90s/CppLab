9af10f54778507ff5985a705695798323581276a
diwen <demon90s@163.com>
2019-12-05 00:49:32 +0800

    commit

--- clabs/signal.c
+++ clabs/signal.c
@@ -12,28 +12,24 @@
     注意：信号由信号处理函数处理时，不可以被重复调用，如果此时又触发了信号，那么对
     该信号的处理要被重置为 SIG_DFL ,或以其他方式加锁。
 
+    不是所有函数都可以在signal处理函数中调用，比如 printf 就不能，结果没有定义。
+    安全的函数请使用 man signal-safety 查阅。
 */
 
-#ifdef __unix
 #include <unistd.h>
-#endif
 #include <signal.h>
 #include <stdio.h>
 
 void handle_sigint(int sig)
 {
-    printf("catch signal %d, reset to default\n", sig);
-
-    /* sleep for a while */
-    #ifdef __unix
-    sleep(3);
-    #endif
+    write(1, "catch signal\n", 13);
 
     signal(SIGINT, SIG_DFL);
 }
 
 int main()
 {
+    // hit ctrl+c to trigger
     signal(SIGINT, handle_sigint);
 
     while (1) {}

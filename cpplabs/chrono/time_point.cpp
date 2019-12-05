17dc18694f40e1780e3445a5aa0631973ebbf4f7
Diwen.Liu <demon90s@163.com>
2019-12-05 20:53:06 +0800

    commit

--- cpplabs/chrono/time_point.cpp
+++ cpplabs/chrono/time_point.cpp
@@ -10,7 +10,7 @@
 #include <chrono>
 #include <iostream>
 
-int main()
+void test_system_clock()
 {
     // current time of system clock
     std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
@@ -19,6 +19,28 @@ int main()
 
     auto timestamp = std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch());
     std::cout << timestamp.count() << std::endl;    // seconds from utc
+}
+
+void test_steady_clock()
+{
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
+    std::cout << "run test steady clock, check duration...\n";
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    auto duration = end - start;
+    std::cout << "duration: " << duration.count() << "(nanoseconds)\n";
+
+    auto duration_micro = std::chrono::duration_cast<std::chrono::microseconds>(duration);
+    std::cout << "duration: " << duration_micro.count() << "(microseconds)\n";
+}
+
+int main()
+{
+    test_system_clock();
+
+    test_steady_clock();
 
     return 0;
 }

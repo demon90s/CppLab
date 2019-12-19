4a65b00f1f80c4de3c9481e0bc112ba0502dfd94
demon90s <demon90s@163.com>
2019-12-19 16:06:25 +0800

    add BenchMarking

---
+++ my_template_lab/test/test_BenchMark.cpp
@@ -0,0 +1,23 @@
+#include "../library/BenchMarking.hpp"
+
+int main()
+{
+    int v = 0;
+    {
+        //BenchMarking<std::chrono::microseconds> bm;
+        BenchMarking<> bm([](std::size_t dur_ms) { std::cout << "BenchMarking finish, duration: " << dur_ms / 1000.0 << " s\n"; });
+        for (int i = 0; i < 100000; i++)
+        {
+            v += i;
+        }
+
+        for (int i = 0; i < 1000; i++)
+        {
+            FILE *fp = tmpfile();
+            fprintf(fp, "hello");
+            fclose(fp);
+        }
+    }
+
+    std::cout << v << std::endl;
+}

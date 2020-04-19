136124fb6606e8cb54e059798c3c77a0a0dea1d3
diwen <demon90s@163.com>
2020-04-19 22:33:21 +0800

    commit

--- cpplabs/misc/rvalue.cpp
+++ cpplabs/misc/rvalue.cpp
@@ -6,21 +6,23 @@ using namespace std;
 
 int func()
 {
-	return 10; // 函数返回一个值是右值
+    return 10; // 函数返回一个值是右值
 }
 
 int main()
 {
-	//func() = 5; // error: lvalue required as left operand of assignment
-	
-	const int &a = func();
-	//int &b = func(); // error: invalid initialization of non-const reference of type ‘int&’ from an rvalue of type ‘int’
+    //func() = 5; // error: lvalue required as left operand of assignment
 
-	const int &c = 5;
-	//int &d = 5; // same as b，5 is a rvalue
+    const int &a = func();
+    //int &b = func(); // error: invalid initialization of non-const reference of type ‘int&’ from an rvalue of type ‘int’
 
-	cout << a << endl;
-	cout << c << endl;
+    const int &c = 5;
+    //int &d = 5; // same as b，5 is a rvalue
 
-	return 0;
+    int &&d = 3 * 4;
+
+    cout << a << endl;
+    cout << c << endl;
+
+    return 0;
 }
